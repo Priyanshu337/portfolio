7dbd69b62bb8c1c3b11d5fed43e7f098e3b8b45a
Priyanshu <priyanshu@Priyanshus-MacBook-Pro.local>
2024-03-04 21:02:17 -0500

    Commit after changes of image and links of Linkedin and Git but images of project are remaining

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export default function footer() {
       <div className="container px-6 pt-6">
         <div className="mb-6 flex justify-center">
           <a
-            href="https://www.linkedin.com/in/manoj-choudhary-060aa2124/"
+            href="https://www.linkedin.com/in/priyanshu0209"
             type="button"
             className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
             data-te-ripple-init
@@ -23,7 +23,7 @@ export default function footer() {
           </a>
 
           <a
-            href="https://github.com/manojown"
+            href="https://github.com/Priyanshu337"
             type="button"
             className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
             data-te-ripple-init
