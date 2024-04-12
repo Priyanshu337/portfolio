75951118d539d6948ca2ddab4f1434ab65e728d9
Priyanshu <priyanshu@Priyanshus-MacBook-Pro.local>
2024-04-12 01:43:52 -0400

    Changes in link of about

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -11,7 +11,7 @@ export default function IndexPage() {
         </h1>
       </div>
       <div className="m-3">
-        <GetMeeshoDetails />
+        <GetAppEaseMDDetails />
       </div>
       <div className="m-3">
         <GetProjectDetails />
@@ -31,7 +31,7 @@ function GetProjectDetails() {
   return (
     <section className="flex-column border-1 m-6 flex h-auto items-center justify-center">
       <div
-        onClick={() => redirection("https://cricket.com/")}
+        onClick={() => redirection("https://hgffd.com/")}
         className="text-side flex h-fit cursor-pointer flex-col rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:shadow-lg-invert md:flex-row"
       >
         <Image
@@ -43,16 +43,16 @@ function GetProjectDetails() {
         />
         <div className="flex flex-col justify-start p-6">
           <h5 className="mb-2 text-center text-xl  font-extrabold text-[#147efb] dark:text-neutral-50 md:text-left">
-           Camera App (Final Project)
+            Camera App (Final Project)
           </h5>
           <p className="mb-2  truncate font-extrabold text-gray-400 dark:text-gray-400">
             Mar,200 - Oct,2021
           </p>
           <p className="max-w-[500px] text-xl font-medium leading-7 text-muted-foreground sm:text-base">
-           
-Designed and developed a Wear OS app in Java using Android Studio, allowing users to capture images seamlessly on their wearable devices. Implemented camera functionality with front and rear camera options, along with flash control for versatile photography in various conditions.
 
-Conducted rigorous testing, debugging, and optimization for a stable and responsive application. Collaborated with cross-functional teams to gather requirements and iterate on features based on user feedback. Ensured compliance with Android development best practices and stayed updated on Wear OS ecosystem innovations.
+            Designed and developed a Wear OS app in Java using Android Studio, allowing users to capture images seamlessly on their wearable devices. Implemented camera functionality with front and rear camera options, along with flash control for versatile photography in various conditions.
+
+            Conducted rigorous testing, debugging, and optimization for a stable and responsive application. Collaborated with cross-functional teams to gather requirements and iterate on features based on user feedback. Ensured compliance with Android development best practices and stayed updated on Wear OS ecosystem innovations.
           </p>
           <div className="stack flex flex-wrap dark:text-white">
             <p>JAVA</p>
@@ -65,11 +65,11 @@ Conducted rigorous testing, debugging, and optimization for a stable and respons
   )
 }
 
-function GetMeeshoDetails() {
+function GetAppEaseMDDetails() {
   return (
     <section className="flex-column border-1 m-6 flex h-auto items-center justify-center dark:shadow-lg-invert">
       <div
-        onClick={() => redirection("https://meesho.com/")}
+        onClick={() => redirection("www.google.com")}
         className="text-side flex h-fit cursor-pointer
 flex-col rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:flex-row"
       >
@@ -89,11 +89,11 @@ flex-col rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rg
             {/* <span className="text-gray-700 dark:text-white">Current</span> */}
           </p>
           <p className="max-w-[500px] text-xl font-medium leading-7 text-muted-foreground sm:text-base">
-          Led full-stack development of a state-of-the-art React-based mobile web app, connecting users with healthcare professionals. Achieved a 30% boost in appointment bookings through user-friendly scheduling features. Implemented Agile processes, reducing time-to-market by 20%.
+            Led full-stack development of a state-of-the-art React-based mobile web app, connecting users with healthcare professionals. Achieved a 30% boost in appointment bookings through user-friendly scheduling features. Implemented Agile processes, reducing time-to-market by 20%.
 
-Integrated Microsoft Chatbot, slashing appointment verification time by 90% and elevating user satisfaction. Improved user engagement by 25% through seamless access to historical and upcoming appointments.
+            Integrated Microsoft Chatbot, slashing appointment verification time by 90% and elevating user satisfaction. Improved user engagement by 25% through seamless access to historical and upcoming appointments.
 
-Designed and managed a robust SQL Database, enhancing data integrity and processing speed by 15%. Maintained code quality with a 95% code coverage through rigorous testing practices.{" "}
+            Designed and managed a robust SQL Database, enhancing data integrity and processing speed by 15%. Maintained code quality with a 95% code coverage through rigorous testing practices.{" "}
           </p>
           <div className="stack flex flex-wrap dark:text-white">
             <p>React</p>
